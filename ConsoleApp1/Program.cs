d4a1272667a48138da518ae883c678e79712eb48
KINU02-PC\KINU04 <bohulukwame@gmail.com>
2020-02-13 14:46:52 +0900

    added missing console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using PdfiumSharp;
+using Newtonsoft.Json;
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        public class TestParams
+        {
+
+            public string pathToFile="";
+
+        }
+
+        public static TestParams LoadJson()
+        {
+            string jsonPath = Environment.GetEnvironmentVariable("JsonFilePath") as string;
+            using (StreamReader r = new StreamReader(jsonPath))
+            {
+                string json = r.ReadToEnd();
+                TestParams param = JsonConvert.DeserializeObject<TestParams>(json);
+                return param;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Pdfium pdf = new Pdfium();
+
+            TestParams param = LoadJson();
+
+
+
+            //string file = path + fileName;
+            string file = param.pathToFile;
+
+            Console.WriteLine("\nOpen PDF file: " + file);
+
+            pdf.LoadFile(file);
+
+            Console.WriteLine("\nNumber of Pages:" + pdf.PageCount().ToString());
+
+            var info = pdf.GetInformation();
+            Console.WriteLine("\nCreator: " + info.Creator);
+            Console.WriteLine("\nTitle: " + info.Title);
+            Console.WriteLine("\nAuthor: " + info.Author);
+            Console.WriteLine("\nSubject: " + info.Subject);
+            Console.WriteLine("\nKeywords: " + info.Keywords);
+            Console.WriteLine("\nProducer: " + info.Producer);
+            Console.WriteLine("\nCreationDate: " + info.CreationDate);
+            Console.WriteLine("\nModDate: " + info.ModificationDate);
+        }
+
+       
+
+       
+    }
+}
