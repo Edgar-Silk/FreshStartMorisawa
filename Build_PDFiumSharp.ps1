a89dd6a7cd3dd091f3bf99c4f204400ac8f3a95f
Edgar-Silk <54256169+Edgar-Silk@users.noreply.github.com>
2020-02-13 12:52:42 +0900

    Add files via upload
    uploaded from PC

---
+++ Build_PDFiumSharp.ps1
@@ -0,0 +1,101 @@
+#initialize parameters
+param(
+    [string] $Arch ='x64',
+
+    [string] $Wrapper_Branch=' '
+)
+
+#ProjectName
+
+$Project_Name ='PDFiumSharp'
+
+#Current Path
+$CurrentPath=(Get-Location).Path
+
+#Display Config
+Write-Host "Architecture: " $Arch
+Write-Host "Directory to Build: " $CurrentPath
+
+#Locate Visual Studio
+function buildVStudio{
+    param(
+    [parameter(Mandatory=$true)]
+    [String] $path,
+
+    [parameter(Mandatory=$false)]
+    [bool] $clean =$true
+    )
+
+    process{
+        $msBuildExe = Resolve-Path "${env:ProgramFiles(x86)}/Microsoft Visual Studio/2017/*/MSBuild/*/bin/msbuild.exe" -ErrorAction SilentlyContinue
+
+        if($clean){
+            Write-Host "Cleaning $($path)" -ForegroundColor blue
+            & "$($msBuildExe)" "$($path)" /t:Clean /m /p:Configuration=Release,Platform=$Arch /v:n
+        }
+
+        Write-Host "Building $($path)" -ForegroundColor blue
+            & "$($msBuildExe)" "$($path)" /t:Build /m /p:Configuration=Release,Platform=$Arch /v:n
+        
+    }
+
+}
+
+buildVStudio -path "$CurrentPath/$Project_Name/$Project_Name.csproj"
+
+
+
+#Check if the pdfium.dll library exists
+Write-Host "Checking for PDFium.dll Library..."
+
+if($Arch -eq 'x64'){
+    $OUT_DLL_DIR = $CurrentPath + '/Lib/x64'
+}elseif($Arch -eq 'x86'){
+    $OUT_DLL_DIR = $CurrentPath + '/Lib/x86'
+}else {
+    Write-Host "Arch not defined or invalid..."
+    Exit
+}
+
+
+#Copy pdfium.dll to appropriate folder
+Write-Host "Copying pdfium.dll to pdfiumSharp Remake solution project.."
+
+$lib_Dir=$CurrentPath+"/"+$Project_Name+"/lib/"+$Arch
+
+if ([System.IO.Directory]::Exists( $lib_Dir )) {
+    Set-Location $lib_Dir
+}
+else {
+    New-Item -Path $lib_Dir -ItemType Directory
+    Set-Location $lib_Dir
+}
+
+if (Test-Path -Path $OUT_DLL_DIR'/pdfium.dll') {
+    Copy-Item $OUT_DLL_DIR'/pdfium.dll' -Destination $lib_Dir
+}
+
+#make nuget package
+Write-Host "Now making Nuget Package..." -ForegroundColor Blue
+
+Set-Location $CurrentPath"/"$Project_Name
+nuget pack PDFiumSharp.csproj -properties "Configuration=Release;Platform=$Arch"
+
+#set directory for nuget package
+$OUT_NUGET_DIR = $CurrentPath+'/NuGet/'+$Arch
+
+if ([System.IO.Directory]::Exists($OUT_NUGET_DIR)) {
+    Set-Location $OUT_NUGET_DIR
+}
+else {
+    New-Item -Path $OUT_NUGET_DIR -ItemType Directory
+    Set-Location $OUT_NUGET_DIR
+}
+
+Write-Host "Copy Nuget files to " $OUT_NUGET_DIR
+
+Copy-Item -Path "$CurrentPath/$Project_Name/*.*.*.*.nupkg" -Destination $OUT_NUGET_DIR
+
+#Copy-Item -Path "$lib_Dir/pdfium.dll" -Destination
+
+Set-Location $CurrentPath
---
+++ The_Test.ps1
@@ -0,0 +1,109 @@
+# PDFium.DLLをビルドするためのスクリプト -- depot_toolsをインクルードする
+# パラメーター
+param (
+    # オプション: x86 | x64
+    [string]$Arch = 'x64',
+    # 予約パラメーター
+    [string]$Wrapper_Branch = ' ',
+
+    [string]$JsonFilePath =(Get-Location).path +'/testparams.json',
+
+    [string]$pathToTestPDF =(Get-Location).path +'/PDFiumSharp.Test/test.pdf'
+)
+
+# Project Name
+$Project_Name = 'PDFiumSharp.Test'
+
+
+# ビルドディレクトリー
+$BuildDir = (Get-Location).path
+
+# コンフィグ
+Write-Host "Architecture: " $Arch
+Write-Host "Directory to Build: " $BuildDir
+
+# Restore NuGet packages. This us used for NUnit3 
+Write-Host "Restore NuGet Packages - Xunit"
+dotnet restore 
+
+# Visual Studio MSI-Builder - コンパイラーを設定する
+Write-Host "Locate VS 2017 MSBuilder.exe"
+function buildVS {
+    param (
+        [parameter(Mandatory=$true)]
+        [String] $path,
+
+        [parameter(Mandatory=$false)]
+        [bool] $clean = $true
+    )
+    process {
+        $msBuildExe = Resolve-Path "${env:ProgramFiles(x86)}/Microsoft Visual Studio/2017/*/MSBuild/*/bin/msbuild.exe" -ErrorAction SilentlyContinue
+
+        if ($clean) {
+            Write-Host "Cleaning $($path)" -foregroundcolor green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m /p:Configuration=Release,Platform=$Arch /v:n
+        }
+
+        Write-Host "Building $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Build /m /p:Configuration=Release,Platform=$Arch /v:n
+    }
+}
+
+# DLLが存在するかチェックしWrapper/Libへコピーする
+Write-Host "Checking for PDFium.DLL library..."
+
+if ($Arch -eq 'x64') {
+    $OUT_DLL_DIR = $BuildDir + '/Lib/x64'
+}
+elseif ($Arch -eq 'x86') {
+    $OUT_DLL_DIR = $BuildDir + '/Lib/x86'
+}
+else {
+    Write-Host "Arch not defined or invalid..."
+    Exit
+}
+
+# solutionをコピーする
+Write-Host "Copy pdfium DLL to PdfiumSharp.Test solution project"
+
+$Lib_Dir = $BuildDir+"/"+$Project_Name+"/lib/"+$Arch
+
+Write-Host "Copy pdfium DLL to PdfiumSharp.Test test directory"
+$test_dir=$BuildDir+"/"+$Project_Name+"/bin/"+$Arch+"/Release/netcoreapp2.1"
+$debug_test_dir=$BuildDir+"/"+$Project_Name+"/bin/Debug/netcoreapp2.1"
+
+if ([System.IO.Directory]::Exists( $Lib_Dir )) {
+    Set-Location $Lib_Dir
+}
+else {
+    New-Item -Path $Lib_Dir -ItemType Directory
+    Set-Location $Lib_Dir
+}
+
+if (Test-Path -Path $OUT_DLL_DIR'/pdfium.dll') {
+    Copy-Item $OUT_DLL_DIR'/pdfium.dll' -Destination $Lib_Dir
+    Copy-Item $OUT_DLL_DIR'/pdfium.dll' -Destination $test_dir
+    Copy-Item $OUT_DLL_DIR'/pdfium.dll' -Destination $debug_test_dir
+
+}
+
+
+# Build the unit test project
+buildVS -path "$BuildDir/$Project_Name/$Project_Name.csproj"
+
+# Make the test
+Set-Location $BuildDir/$Project_Name
+
+Write-Host "start dotnet test..." -foregroundcolor green
+ 
+ $a = Get-Content $JsonFilePath | ConvertFrom-Json
+ $a.pathToFile=$pathToTestPDF
+ $a | ConvertTo-Json | set-content $JsonFilePath
+ #($env:JsonFilePath=$JsonFilePath) | 
+ dotnet vstest "bin/$Arch/Release/netcoreapp2.1/$Project_Name.dll" 
+
+
+
+Set-Location $BuildDir
+
+ls
